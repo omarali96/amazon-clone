94efd5c79fbbd76f5ca5183e919994593f3a3a60
Mahmoud Elshall <mahmoudelshall292@gmail.com>
2023-06-10 03:08:13 +0300

    complete filter and sorting functionality

--- js/index/shop.js
+++ js/index/shop.js
@@ -9,197 +9,123 @@ const APIs = {
   AllProducts: "http://localhost:5000/api/products",
 }; // APIs
 let producterContainer = [];
-  const featProducts = await fetchAPI(APIs.AllProducts, {}, {}, "GET");
+let filteredProduct = [];
+const featProducts = await fetchAPI(APIs.AllProducts, {}, {}, "GET");
 for (const product of featProducts.data) {
-    let objProduct = new Product(product); 
-    producterContainer.push(objProduct);
+  let objProduct = new Product(product);
+  producterContainer.push(objProduct);
 }
 let htmlProductContainer = document.getElementById("productContainer");
 
 for (const key in producterContainer) {
-  htmlProductContainer.innerHTML += producterContainer[key].displayProductCart(key);  
+  htmlProductContainer.innerHTML += producterContainer[key].displayProductCart(key);
 }
 // filter fun 
-let filterPrice = function(element, prices){
-
+let filterPrice = function (element, prices) {
+  if (prices.includes("all")) return true;
   for (const item of prices) {
-    if( (Number(element.discountedPrice) >= Number(item)) && (Number(element.discountedPrice) <= (Number(item)+100)) ) return true
+    if ((Number(element.discountedPrice) >= Number(item)) && (Number(element.discountedPrice) <= (Number(item) + 100))) return true
   }
   return false;
 }
 
-let filterColer = function(element, colors){
+let filterColer = function (element, colors) {
+  if (colors.includes("all")) return true;
   for (const item of colors) {
-    if(element.color.toLocaleLowerCase() == item.toLocaleLowerCase() ) return true 
+    if (element.color.toLocaleLowerCase() == item.toLocaleLowerCase()) return true
   }
   return false;
 }
-let filterSize = function(element, sizes){
+let filterSize = function (element, sizes) {
+  if (sizes.includes("all")) return true;
   for (const item of sizes) {
-    if(element.size.toLocaleLowerCase() == item.toLocaleLowerCase() ) return true  
+    if (element.size.toLocaleLowerCase() == item.toLocaleLowerCase()) return true
   }
   return false;
 }
+
+
 
 // get checked value from check pox elements
-let getCheckboxValue= function (elementsName, elementLength) {
+let getCheckboxValue = function (elementsName, elementLength) {
   document.getElementsByName(elementsName)[0]
-  let pricesValueArray= [];
-  for(let i= 0; i< elementLength; i++){
-    if (document.getElementsByName(elementsName)[i].checked == true){
+  let pricesValueArray = [];
+  for (let i = 0; i < elementLength; i++) {
+    if (document.getElementsByName(elementsName)[i].checked == true) {
       let v = document.getElementsByName(elementsName)[i].value;
       pricesValueArray.push(v);
     }
   }
-  if(pricesValueArray.length == 0){
-      pricesValueArray.push('all');
+  if (pricesValueArray.length == 0) {
+    pricesValueArray.push('all');
   }
   return pricesValueArray;
 }
 
-
-
-
-console.log(producterContainer);
-// get product container in html and add all product
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let filteredProduct = producterContainer.filter(element=>{
- return filterPrice(element, [0,100,200,800,900]) && filterColer(element, ["white","green"]) && filterSize(element,["m","xs","s"])
+let renderProductWithFilter = function () {
+  // get checked prices 
+  let priceArray = getCheckboxValue('price', 6);
+  let colorArray = getCheckboxValue('color', 6);
+  let sizeArray = getCheckboxValue('size', 6);
+  filteredProduct = producterContainer.filter(element => {
+    return filterPrice(element, priceArray) && filterColer(element, colorArray) && filterSize(element, sizeArray)
+  });
+  htmlProductContainer.innerHTML = "";
+  for (const key in filteredProduct) {
+    htmlProductContainer.innerHTML += filteredProduct[key].displayProductCart(key);
+  }
+}
+// filter action on element 
+document.getElementById("priceForm").addEventListener('click', function (e) {
+  e.preventDefault();
+  renderProductWithFilter();
+});
+document.getElementById("colorForm").addEventListener('click', function (e) {
+  e.preventDefault();
+  renderProductWithFilter();
+});
+document.getElementById("sizeForm").addEventListener('click', function (e) {
+  e.preventDefault();
+  renderProductWithFilter();
 });
 
-console.log(filteredProduct);
+// sort action on element 
+document.getElementById("sortPrice").addEventListener('click', function (e) {
+  e.preventDefault();
+  if(filteredProduct.length == 0){
+    filteredProduct = producterContainer.map(e=>e);
+  }
+  filteredProduct.sort((a, b) => a.price > b.price ? 1 : b.price > a.price ? -1 : 0);
+  htmlProductContainer.innerHTML = "";
+  for (const key in filteredProduct) {
+    htmlProductContainer.innerHTML += filteredProduct[key].displayProductCart(key);
+  }
 
+});
 
+document.getElementById("sortPopularity").addEventListener('click', function (e) {
+  e.preventDefault();
+  if(filteredProduct.length == 0){
+    filteredProduct = producterContainer.map(e=>e);
+  }
+  filteredProduct.sort((a, b) => a.ratingCount < b.ratingCount ? 1 : b.ratingCount < a.ratingCount ? -1 : 0);
+  htmlProductContainer.innerHTML = "";
+  for (const key in filteredProduct) {
+    htmlProductContainer.innerHTML += filteredProduct[key].displayProductCart(key);
+  }
 
+});
+document.getElementById("sortRating").addEventListener('click', function (e) {
+  e.preventDefault();
+  if(filteredProduct.length == 0){
+    filteredProduct = producterContainer.map(e=>e);
+  }
+  filteredProduct.sort((a, b) => a.rating < b.rating ? 1 : b.rating < a.rating ? -1 : 0);
+  htmlProductContainer.innerHTML = "";
+  for (const key in filteredProduct) {
+    htmlProductContainer.innerHTML += filteredProduct[key].displayProductCart(key);
+  }
 
+});
 
-
-
-
-
-
-//let priceValues  = document.getElementsByName("price")[0].value
-
-
-    
-//console.log(getCheckboxValue("size",6));
-
-//   if (lang1.checked == true){
-//     let lg1= document.getElementById("s1").value;
-//     result= lg1 + " ";
-//   }
-//   else if (lang2.checked == true){
-//     let lg2= document.getElementById("s2").value;
-//     result= result + lg2 + " ";
-//   }
-//   else if (lang3.checked == true){
-//   document.write(result);
-//     let lg3= document.getElementById("s3").value;
-//     result= result + lg3 ;
-//   }
-//    else {
-//   return document.getElementById("result").innerHTML= "Select any one";
-//   }
-//   return document.getElementById("result").innerHTML= "You have selected " + result + " language";
-// }
-
-
-
-
-
-
-
-
-
-// const filterProducts = (
-//   f1 = true,
-//   f2 = true,
-//   f3 = true,
-//   f4 = true,
-//   f5 = true,
-//   products,
-//   filterCriteria
-// ) => {
-//   if (!Array.isArray(products) || products.length === 0)
-//     throw new Error("Error: products is invalid");
-//   let filteredProducts = new Array();
-//   switch (filterCriteria) {
-//     case "price":
-//       filteredProducts = products.filter(
-//         (product) =>
-//           (f1 && product.price <= 100) ||
-//           (f2 && product.price > 100 && product.price <= 200) ||
-//           (f3 && product.price > 200 && product.price <= 300) ||
-//           (f4 && product.price > 300 && product.price <= 400) ||
-//           (f5 && product.price > 400 && product.price <= 500)
-//       );
-//       break;
-//     case "color":
-//       filteredProducts = products.filter(
-//         (product) =>
-//           (f1 && product.color === "black") ||
-//           (f2 && product.color === "white") ||
-//           (f3 && product.color === "blue") ||
-//           (f4 && product.color === "red") ||
-//           (f5 && product.color === "green")
-//       );
-//       break;
-//     case "size":
-//       filteredProducts = products.filter(
-//         (product) =>
-//           (f1 && product.size === "XS") ||
-//           (f2 && product.size === "S") ||
-//           (f3 && product.size === "M") ||
-//           (f4 && product.size === "L") ||
-//           (f5 && product.size === "XL")
-//       );
-//     default:
-//       break;
-//   }
-//   return filteredProducts;
-// };
-
-// const sortProducts = (products, sortCriteria) => {
-//   let sortedProducts = new Array();
-//   switch (sortCriteria) {
-//     case "price":
-//       sortedProducts = products.sort((a, b) => {
-//         return a.price - b.price;
-//       });
-//       break;
-//     case "popularity":
-//       sortedProducts = products.sort((a, b) => {
-//         return b.popularity - a.popularity; // TODO: sort by popularity
-//       });
-//       break;
-//     case "rating":
-//       sortedProducts = products.sort((a, b) => {
-//         return b.rating - a.rating;
-//       });
-//     default:
-//       break;  
-//   }
-//   return sortedProducts;
-// };
-// const showingProducts = (products, numberOfProducts) => {
-//   return products.slice(0, numberOfProducts);
-// };
-// const calculatePages = (products, numberOfProducts) => {
-//   return Math.ceil(products.length / numberOfProducts);
-// };
-
-
-
+// pagination and showing menu 
